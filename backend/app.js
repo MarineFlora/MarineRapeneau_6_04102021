1554629f1d195797a886fa49c6240062b39bbd45
MarineFlora <marine.rapeneau@gmail.com>
2021-10-04 18:10:07 +0200

    connexion base de donnée et ajout fichier .env

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,25 @@
 const express = require('express');
 // import du package body-parser pour extraire l'objet JSON des demandes POST
 const bodyParser = require('body-parser');
+// import de mongoose
+const mongoose = require('mongoose');
+// import path, donne accès au chemin de notre système de fichier
+const path = require('path');
+// import fichier config
+const config = require('./config.js');
+
+//import des router
+
 
 // création de l'application express
 const app = express();
+
+//connecter base de donnée à mongoose pour faciliter interaction 
+mongoose.connect(`mongodb+srv://${config.MONGO_DB_USERNAME}:${config.MONGO_DB_PASSWORD}@cluster0.umj8q.mongodb.net/${config.MONGO_DB_NAME}?retryWrites=true&w=majority`, 
+    { useNewUrlParser : true,
+    useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 // middleware pour résoudre problèmes de CORS et permettre à l'accès à l'API
 app.use((req, res, next) => {
