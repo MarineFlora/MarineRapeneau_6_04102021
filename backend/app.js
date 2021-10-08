6f71bf56e410a2a11d009f402f6bcbbf607298f4
MarineFlora <marine.rapeneau@gmail.com>
2021-10-08 14:16:34 +0200

    ajout gestionnaire routage vers /images

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,7 @@ const config = require('./config.js');
 //import des router
 const userRoutes = require('./routes/user');
 const saucesRoutes = require('./routes/sauces');
+// route like
 
 // création de l'application express
 const app = express();
@@ -23,7 +24,7 @@ mongoose.connect(`mongodb+srv://${config.MONGO_DB_USERNAME}:${config.MONGO_DB_PA
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-// middleware pour résoudre problèmes de CORS et permettre à l'accès à l'API
+// middleware pour résoudre problèmes de CORS et permettre l'accès à l'API
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -33,6 +34,10 @@ app.use((req, res, next) => {
 
 //middleware global, transforme le corps de la requete en objet javascript utilisable
 app.use(express.json());
+
+// indique à Express qu'il faut gerer la ressource images de manière statique à chaque requête reçue vers la route /images
+// __dirname = nom du dossier dans lequel on va se trouver
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 // enregistrement du routeur avec racine attendue par front-end
 app.use('/api/auth', userRoutes);
--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -14,20 +14,20 @@ exports.createSauce = (req, res, next) => {
     delete sauceObject._id;
     // nouvelle instance de l'objet Sauce en lui passant un objet JS contenant les infos requise du corps de requete analysé
     const sauce = new Sauce({
-    // on utlise l'opérateur spread ... pour faire une copie de tous les élements req.body
-    // va copier les champs qu'il y a dans le corps de la requete et détailler le titre, le description etc
-    ...sauceObject,
-    // configuration de l'url de l'image
-    // par : "protocole, http/https"://"racineduserveur(localhost:3000)"/images/.fichier.nomfichier
-    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}` 
+        // on utlise l'opérateur spread ... pour faire une copie de tous les élements req.body
+        // va copier les champs qu'il y a dans le corps de la requete et détailler le titre, le description etc
+        ...sauceObject,
+        // configuration de l'url de l'image
+        // par : "protocole, http/https"://"racineduserveur(localhost:3000)"/images/nomfichier(configuré par multer)
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}` 
    });
    // Enregistrement de la sauce dans la base de données
    // retourne une promise
     sauce.save()
-    // on renvoi un code 201 pour une bonne création de ressource
-    .then(() => res.status(201).json({ message: 'Sauce enregistrée !'}))
-    // on récupère l'erreur avec un code 400
-    .catch(error => res.status(400).json({ error }));
+        // on renvoi un code 201 pour une bonne création de ressource
+        .then(() => res.status(201).json({ message: 'Sauce enregistrée !' }))
+        // on récupère l'erreur avec un code 400
+        .catch(error => res.status(400).json({ error })); 
 };
 
 // modification d'une sauce par son id
@@ -42,10 +42,21 @@ exports.deleteSauce = (req, res, next) => {
 
 // récupération de toutes les sauces
 exports.getAllSauces = (req, res, next) => {
-
+    // methode find pour obtenir la liste complète
+    // retrourne une promise
+    Sauce.find()
+        // on récupère le tableau de toutes les sauces retournées dans la base données
+        .then(sauces => res.status(200).json(sauces))
+        .catch(error => res.status(400).json({ error }));
 };
 
 // récupération d'une sauce par son id 
 exports.getOneSauce = (req, res, next) => {
-
+    // findOne = trouve un seul
+    // on veut que l'id de la sauce soit le même que le paramètre de requete
+    Sauce.findOne({ _id: req.params.id })
+        // cette sauce est retournée dans une promise et envoyée au front-end
+        .then(sauce => res.status(200).json(sauce))
+        // si aucune sauce trouvée, on envoi erreur 404
+        .catch(error => res.status(404).json({ error }));
 };
