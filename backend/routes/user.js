3075bd60c70abaa85e810fc6c4631b15e772274d
MarineFlora <marine.rapeneau@gmail.com>
2021-10-19 14:31:13 +0200

    install ajv pour exiger bon email et mot de passe fort

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,10 +7,12 @@ const router = express.Router();
 
 // import du controller user
 const userCtrl = require('../controllers/user');
+// import middleware email correct et mot de passe fort
+const checkInputs =  require('../middleware/check-inputs');
 
 // routes POST car le front-end envoi l'adresse mail et mdp
 // (adresse indiquée = le segment final, le reste de l'adresse est déclaré dans appli express)
-router.post('/signup', userCtrl.signup);
+router.post('/signup', checkInputs, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 //on exporte pour pouvoir importer dans app.js
